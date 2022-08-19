8141f439d5887ac17bab769487dba3b49f313baf
Pratik Khasnabis <pratik.khasnabis@outlook.com>
2022-08-19 13:54:02 +1000

    initial commit

---
+++ main.tf
@@ -0,0 +1,37 @@
+
+# https://docs.aws.amazon.com/vpc/latest/userguide/amazon-vpc-limits.html#vpc-limits-security-groups
+
+locals {
+  # Read json files and append them into a single directory
+  inputfiles = [for f in fileset(path.module, "JsonObjects/*_objects.json") : jsondecode(file(f))]
+
+  security_group_resources = merge(local.inputfiles...)
+}
+
+# For test
+module "web_server_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "4.11.0"
+
+  create = false
+
+  name        = "web-server"
+  description = "Security group for web-server with HTTP ports open within VPC"
+  vpc_id      = var.vpc_id
+
+  ingress_cidr_blocks = ["10.100.0.0/32", "10.100.0.1/32"]
+}
+
+# create security group
+module "appid_sg" {
+  source  = "terraform-aws-modules/security-group/aws//modules/http-80"
+  version = "4.11.0"
+
+  create = true
+
+  for_each            = local.security_group_resources
+  name                = each.key
+  description         = "Security group from Json"
+  vpc_id              = var.vpc_id
+  ingress_cidr_blocks = lookup(each.value, "ipaddr", [])
+}
---
+++ variables.tf
@@ -0,0 +1,7 @@
+variable "region" {
+  default = "ap-southeast-2"
+}
+
+variable "vpc_id" {
+  description = "The Id of the VPC where the Secutiy Group resources will be deployed"
+}
